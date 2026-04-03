07213b1acd692285a66b009a5932739f98bd9bc4
pheetomilechi <tomilechighajiga@gmail.com>
2026-04-03 16:44:00 +0100

    first commit

---
+++ day1.py
@@ -0,0 +1,7 @@
+""""
+This is my Day 1 Python File.
+Created for the Innovempia training program
+"""
+# This prints my first message
+print("Hello world")
+print("I am a backend developer in training.")
---
+++ variables.py
@@ -0,0 +1,5 @@
+name = 'Sarah'
+age = '25'
+
+print("My name is " + name)
+print('I am ' + str(age) + " years old")
