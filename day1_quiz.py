73e3c4b259417eda5269d00839a3d31e9fecebd3
pheetomilechi <tomilechighajiga@gmail.com>
2026-04-03 17:44:54 +0100

    Worked on to files by performing a task and did a quiz by providind answers to questions

---
+++ day1_quiz.py
@@ -0,0 +1,20 @@
+# 1. Programming is a way of writng logic tasks via codes for a computer to perfrom. 
+# It is a way of writing set of instructions/commands for a computer to perform specific tasks.
+# 2. Using Resturant analogy, backend is said to be a way, at which a customer enters a resturant and the customer is been attended to
+# by a resturant attendant who takes the order of the customer which is written on a jotter by the resturant attendant, who then proceeds
+# to the kitchen to process the customer's order. After the order is given in the kitchen, a return by the attendant is a meal to the customer.
+# 3. Adding python.exe to path. It is important for giving your operatin system a shortcut to find python interpreter from any location.
+# 4. The three extension we installed were;
+# - Python
+# - Pylance
+# - Python indent
+# 5. The three ways to run python code are;
+# - Using vs code play button
+# - VS code integrated terminal
+# -  System terminal
+# 6. city = New York
+# 7. str()
+# 8. Single-line command: #, Multi-line command: (""""""""")
+# 9. Git is a version control system (VCS) that runs locally on your computer while Github is a cloud-based hosting service for git repositories.
+# 10. python-learning
+ 
---
+++ project_day1.py
@@ -0,0 +1,16 @@
+""""
+Day 1 project for Innovempia
+"""
+Myname = "Tomilechi"
+careername = "Backend Developer"
+companyname = "Innovempia"
+
+print("=" * 40)
+print("MY PYTHON JOURNEY CARD")
+
+print("Name: " + Myname)
+print("Career: " + careername)
+print("Company: " + companyname)
+print("I learned installing Python, running code in the terminal, creating variables, setting up GitHub")
+print("Ready for Day 2!")
+print("=" * 40)
