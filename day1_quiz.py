9c54c1a4ffaded411db5e246e187b52cfcd62713
pheetomilechi <tomilechighajiga@gmail.com>
2026-04-06 01:34:57 +0100

    Made some syntax correction

--- day1_quiz.py
+++ day1_quiz.py
@@ -1,8 +1,7 @@
 # 1. Programming is a way of writng logic tasks via codes for a computer to perfrom. 
 # It is a way of writing set of instructions/commands for a computer to perform specific tasks.
-# 2. Using Resturant analogy, backend is said to be a way, at which a customer enters a resturant and the customer is been attended to
-# by a resturant attendant who takes the order of the customer which is written on a jotter by the resturant attendant, who then proceeds
-# to the kitchen to process the customer's order. After the order is given in the kitchen, a return by the attendant is a meal to the customer.
+# 2. Using the restaurant analogy, the backend is the kitchen. It is where the chefs, recipes, and pantry (database) are located. 
+# It handles the behind-the-scenes work that the customer cannot see.
 # 3. Adding python.exe to path. It is important for giving your operatin system a shortcut to find python interpreter from any location.
 # 4. The three extension we installed were;
 # - Python
@@ -12,9 +11,9 @@
 # - Using vs code play button
 # - VS code integrated terminal
 # -  System terminal
-# 6. city = New York
+# 6. city = "New York"
 # 7. str()
-# 8. Single-line command: #, Multi-line command: (""""""""")
+# 8. Single-line command: #, Multi-line command: ("""""")
 # 9. Git is a version control system (VCS) that runs locally on your computer while Github is a cloud-based hosting service for git repositories.
 # 10. python-learning
  
