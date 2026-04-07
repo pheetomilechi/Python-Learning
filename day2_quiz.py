75d6f955f470106018089b6c81319392f5fefbb4
pheetomilechi <tomilechighajiga@gmail.com>
2026-04-07 02:57:17 +0100

    Completed day2  learning

---
+++ day2_quiz.py
@@ -0,0 +1,25 @@
+# Save As is a manual, clunky way to protect your work. Git is an automated, searchable, and collaborative 
+# safety net that lets you focus on creating rather than managing files.
+# Set your name:
+# git config --global user.name "Your Name"
+# Set your email:
+# git config --global user.email "youremail@example.com", Basically, Git refuses to let you record history anonymously because, 
+# in a team setting, knowing who made a change is just as important as knowing what was changed.
+# Repository (or "repo" for short) is a digital storage space for your project. It can be local to a folder on your computer or hosted 
+# on a platform like GitHub.
+# A Commit is like saving a note in your personal diary, while a Push is like publishing that diary entry to a blog for the world to see.
+# In Git, "Origin" is simply the nickname for the remote repository where you originally got your code from (or where you intend to send it).
+# Think of a Git Revert as a "Correction" rather than an "Undo."
+# When you revert a change, you aren't grabbing an eraser and scrubbing the past out of existence. Instead, you are telling Git:
+# "I made a mistake in that specific version, so please create a brand-new version that does the exact opposite."
+# Think of a Git Rebase as "rewriting the timeline" of your project.
+# While a Merge takes two different paths and ties them together with a knot at the end, a Rebase takes your entire path, 
+# picks it up, and moves it so it starts from a newer point in time. This can make the history cleaner and easier to understand, 
+# but it also means you're changing the past, which can be risky if others are relying on that history.
+# To create a new branch in Git, you can use the command: git checkout -b new-branch-name. 
+# To switch to an existing branch, you can use: git checkout existing-branch-name.
+# A 'Fork' is a server-side copy that lives on your GitHub account so you can safely experiment with someone else's project, 
+# while 'Cloning' is the act of downloading a repository (either your own or a fork) onto your local computer so you can actually write 
+# and run the code.
+# Pull Request (PR) is a "Request for Approval." It is a dedicated space on a platform like GitHub where a developer can show their proposed
+# changes to the rest of the team before those changes are officially added to the main project.
