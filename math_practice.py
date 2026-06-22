2904e7bbf1554efc377a31e0af80ffa9ab258c56
pheetomilechi <tomilechighajiga@gmail.com>
2026-06-22 11:35:52 +0100

    carrying out some random logical operation tests

---
+++ math_practice.py
@@ -0,0 +1,30 @@
+# print(10 + 5) # Addition
+# print(10 - 5) # Subtraction
+# print(10 * 5) # Multiplication
+# print(10 / 3) # Standard Division
+# print(10 // 3) # Floor Division
+# print(10 % 3) # Modulo
+# print(10 ** 2) # Exponentiation
+
+# print(10 % 2)
+# print(11 % 2)
+
+# print(5 == 5)
+# print(5 != 3)
+# print(10 > 5)
+# print(5 < 10)
+# print(5 >= 5)
+# print(5 <= 4)
+
+age = 20
+has_ticket = True
+
+can_enter = age >=18 and has_ticket == True
+print(can_enter)
+
+William = age <= 17 and has_ticket == True
+print(William)
+
+is_vip = False
+can_skip_line = is_vip or age > 65
+print(can_skip_line)
