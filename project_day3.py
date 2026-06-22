010b930e562c0424e3e68057251beca4d6d11811
pheetomilechi <tomilechighajiga@gmail.com>
2026-06-22 10:52:22 +0100

    fixed some bugs in my day 3 projects

--- project_day3.py
+++ project_day3.py
@@ -1,27 +1,28 @@
 
-# === YEAR 3000 SCI-FI STORY ===
-print("In the bustling metropolis of [city], a legend was born")
-print("His name was [hero], and he carried the mighty [weapon]")
-print("At his young ag of [age], he discovered a secret code: [code].")
-print("Was it a real numeric code [True/False].")
-print("Exactly 100 years later, at age [future_age], [hero] saved the universe.")
-print("The name [hero] appears 2 times in this tale!")
 
 hero = input("Enter name: ")
-print(hero.strip())
-print(hero.title())
+hero = hero.strip().title()
+
 
 city = input("Enter city: ")
-print(city.strip())
-print(city.title())
+city = city.strip().capitalize()
 
 age = int(input("Enter age: "))
 future_age = age + 100
 
 weapon = input("Enter a weapon: ")
-print(weapon.upper())
+weapon = weapon.upper()
 
 secret_code = input("Enter a 4 digit code: ")
 is_valid = secret_code.isdigit()
-print("Game has Started")
+
+
+print("=== YEAR 3000 SCI-FI STORY ===")
+print("In the bustling metropolis of " + city + ", a legend was born")
+print("His name was " + hero + ", and he carried the mighty " + weapon + ".")
+print("At his young age of " + str(age) + ", he discovered a secret code: " + secret_code + ".")
+print("Was it a real numeric code " + str(is_valid) + ".")
+print("Exactly 100 years later, at age " + str(future_age) + ", " + hero + " saved the universe.")
+print("The name " + hero + " appears 2 times in this tale!")
+
 
