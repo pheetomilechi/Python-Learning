f4b35a5e76467994304eb6b58663263a3bd89616
pheetomilechi <tomilechighajiga@gmail.com>
2026-06-22 12:58:28 +0100

    Tested my cart condition and it worked

--- project_day4.py
+++ project_day4.py
@@ -6,6 +6,7 @@ age = age.__float__()
 
 total_purchase = float(input("Enter total purchase: "))
 
+
 if age < 13:
     print("Sorry " + name +", you must be 13 or older to shop here.")
 else:
@@ -18,3 +19,14 @@ elif total_purchase >= 50:
     print("Congratulations " + name + "! You have earned a 10% discount on your purchase.")
     total_purchase = total_purchase * 0.9
     print("Your updated total is $" + str(total_purchase) + ".")
+else:
+    if total_purchase < 50:
+        print("Sorry " + name + ", you do not qualify for a discount on your purchase.")
+        
+
+print("INNOVEMPIA STORE RECEIPT ")
+print("Customer Name: " + name)
+print("Original Total: $" + str(total_purchase))
+print("Discount Applied: " + "20%")
+print("Final Total: " + str(total_purchase * 0.8))
+print("Thank you for shopping with us!") 
