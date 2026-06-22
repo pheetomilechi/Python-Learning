fa7bdfaae1c1adf4e34c27a48d4272a25b23475c
pheetomilechi <tomilechighajiga@gmail.com>
2026-06-22 15:40:56 +0100

    Made the code to print the receipt and follow the conditional statements

--- project_day4.py
+++ project_day4.py
@@ -2,31 +2,26 @@ name = input("Enter name: ")
 name = name.strip().title()
 
 age = int(input("Enter age: "))
-age = age.__float__()
+age = float(age)
+original_total = float(input("Enter total purchase: "))
 
-total_purchase = float(input("Enter total purchase: "))
+total_purchase = original_total
 
+discount_applied = "0%"
 
 if age < 13:
-    print("Sorry " + name +", you must be 13 or older to shop here.")
+    print("Sorry " + name + ", you must be 13 or older to shop here.")
 else:
-    print("Welcome " + name + "! Your total purchase is $" + str(total_purchase) + ".")
-if total_purchase >= 100:
-    print("Congratulations " + name + "! You have earned a 20% discount on your purchase.")
-    total_purchase = total_purchase * 0.8
-    print("Your updated total is $" + str(total_purchase) + ".")
-elif total_purchase >= 50:
-    print("Congratulations " + name + "! You have earned a 10% discount on your purchase.")
-    total_purchase = total_purchase * 0.9
-    print("Your updated total is $" + str(total_purchase) + ".")
-else:
-    if total_purchase < 50:
-        print("Sorry " + name + ", you do not qualify for a discount on your purchase.")
-        
-
-print("INNOVEMPIA STORE RECEIPT ")
-print("Customer Name: " + name)
-print("Original Total: $" + str(total_purchase))
-print("Discount Applied: " + "20%")
-print("Final Total: " + str(total_purchase * 0.8))
-print("Thank you for shopping with us!") 
+    if total_purchase >= 100:
+        discount_applied = "20%"
+        total_purchase *= 0.8
+    elif total_purchase >= 50:
+        discount_applied = "10%"
+        total_purchase *= 0.9
+    
+    print("INNOVEMPIA STORE RECEIPT")
+    print("Customer Name: " + name)
+    print("Original Total: $" + str(original_total))
+    print("Discount Applied: " + discount_applied)
+    print("Final Total: $" + str(total_purchase))
+    print("Thank you for shopping with us!")
