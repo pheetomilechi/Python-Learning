b45024435a33f138fe295cc8da559e6decdda111
pheetomilechi <tomilechighajiga@gmail.com>
2026-06-24 12:03:15 +0100

    Answered number 1 - 7

--- day_4_quiz.py
+++ day_4_quiz.py
@@ -1,2 +1,8 @@
-# 1. Standard division always returns a float (decimal) while floor division returns an integer (whole number) result.
-# 11 / 2 = 5.5, 11 // 2 = 5
+# 1. Standard division always returns a float (decimal) while floor division returns an integer (whole number) result. 11 / 2 = 5.5, 11 // 2 = 5
+# 2. Modulo operator returns the remainder of a division operation. 12 % 5 = 2 
+# 3. Because age is a variable and 20 is an integer, the comparison operator < is used to check if the value of age is less than 20. If age is less than 20, the condition evaluates to True; otherwise, it evaluates to False.
+# 4. x = 5, y = 10, x > 3; returns True, y < 5; returns False. This is because the comparison operator > checks if x is greater than 3, which is true, while the comparison operator < checks if y is less than 5, which is false.
+# 5. x = 5, y = 10, x > 3; returns True, y < 5; returns False. This is because the comparison operator > checks if x is greater than 3, which is true, while the comparison operator < checks if y is less than 5, which is false.
+# 6. The purpose of elif statement is to provide an additional condition to check if the previous if statement is false. It allows for multiple conditions to be checked in a single block of code, and only one of the conditions will be executed. If the if statement is false, the program will check the elif statement, and if that is also false, it will move on to the next elif statement or the else statement if there are no more elif statements.
+# 7. If you write an if statement in Python but forget to indent the block of code beneath it, Python will immediately stop running your script and throw a IndentationError.
+# 8. 
