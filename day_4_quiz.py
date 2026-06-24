206ccbc94c472a5fc2c865aeeb4052f7d70f2046
pheetomilechi <tomilechighajiga@gmail.com>
2026-06-24 11:04:36 +0100

    Added a day 4 quiz file and tested a math function

---
+++ day_4_quiz.py
@@ -0,0 +1,2 @@
+# 1. Standard division always returns a float (decimal) while floor division returns an integer (whole number) result.
+# 11 / 2 = 5.5, 11 // 2 = 5
--- math_practice.py
+++ math_practice.py
@@ -1,52 +1,53 @@
-print(10 + 5) # Addition
-print(10 - 5) # Subtraction
-print(10 * 5) # Multiplication
-print(10 / 3) # Standard Division
-print(10 // 3) # Floor Division
-print(10 % 3) # Modulo
-print(10 ** 2) # Exponentiation
+# print(10 + 5) # Addition
+# print(10 - 5) # Subtraction
+# print(10 * 5) # Multiplication
+# print(10 / 3) # Standard Division
+# print(10 // 3) # Floor Division
+# print(10 % 3) # Modulo
+# print(10 ** 2) # Exponentiation
 
-print(10 % 2)
-print(11 % 2)
+# print(10 % 2)
+# print(11 % 2)
 
-print(5 == 5)
-print(5 != 3)
-print(10 > 5)
-print(5 < 10)
-print(5 >= 5)
-print(5 <= 4)
+# print(5 == 5)
+# print(5 != 3)
+# print(10 > 5)
+# print(5 < 10)
+# print(5 >= 5)
+# print(5 <= 4)
 
-age = 20
-has_ticket = True
+# age = 20
+# has_ticket = True
 
-can_enter = age >=18 and has_ticket == True
-print(can_enter)
+# can_enter = age >=18 and has_ticket == True
+# print(can_enter)
 
-William = age <= 17 and has_ticket == True
-print(William)
+# William = age <= 17 and has_ticket == True
+# print(William)
 
-is_vip = False
-can_skip_line = is_vip or age > 65
-print(can_skip_line)
+# is_vip = False
+# can_skip_line = is_vip or age > 65
+# print(can_skip_line)
 
-age = 20
+# age = 20
 
-if age >= 18:
-    print("You are allowed to enter.")
+# if age >= 18:
+#     print("You are allowed to enter.")
     
-    age = 15
+#     age = 15
     
-    if age >= 18:
-        print("You are allowed to enter.")
-    else:
-        print("You are too young to enter.")
+#     if age >= 18:
+#         print("You are allowed to enter.")
+#     else:
+#         print("You are too young to enter.")
 
-grade = 85
-if grade >= 90:
-    print("A")
-elif grade >= 80:
-    print("B")
-elif grade >= 70:
-    print("C")
-else:
-    print("F")
+# grade = 85
+# if grade >= 90:
+#     print("A")
+# elif grade >= 80:
+#     print("B")
+# elif grade >= 70:
+#     print("C")
+# else:
+#     print("F")
+
