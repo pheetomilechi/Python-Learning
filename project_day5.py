aa2408c133ab4de7ec6b9a19d76ecbc1048417e6
pheetomilechi <tomilechighajiga@gmail.com>
2026-06-29 12:02:39 +0100

    Carrying out project day 5 which invloves loop, both if and while loop, break and continue. I created a project_day5 python file and inputed my codes. I am building a security logic backend logic system

---
+++ project_day5.py
@@ -0,0 +1,78 @@
+
+user = 'admin'
+correct_password = "admin1234"
+
+is_locked = True
+if is_locked == True:
+    print(False)
+    
+    for i in range(3):
+        
+        if i == 2:
+            attempt = input("Enter your password: ")
+            is_locked = False
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# count = 1
+
+# while count <= 3:
+#     print("This is loop number " + str(count))
+#     count = count + 1
+    
+#     print("Loop finished!")
+
+# count = 1
+# while count <= 3:
+#     print("Stuck forever!")
+
+
+# for i in range(4):
+#     print(i)
+
+# for i in range(2,6):
+#     print(i)
+
+# for i in range(0, 10, 2):
+#     print(i)
+
+# for i in range(5):
+
+#     if i == 3:
+#         break
+
+#     print(i)
+
+# for i in range(5):
+    
+#     if i == 2:
+#         continue
+#     print(i)
