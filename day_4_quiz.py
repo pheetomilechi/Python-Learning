4d990db956098fad4e9ca3fcbe3e482b358a61f4
pheetomilechi <tomilechighajiga@gmail.com>
2026-07-02 15:22:18 +0100

    Made some corrections to day 4 quiz

--- day_4_quiz.py
+++ day_4_quiz.py
@@ -1,11 +1,11 @@
 # 1. Standard division always returns a float (decimal) while floor division returns an integer (whole number) result. 11 / 2 = 5.5, 11 // 2 = 5
 # 2. Modulo operator returns the remainder of a division operation. 12 % 5 = 2 
-# 3. Because age is a variable and 20 is an integer, the comparison operator < is used to check if the value of age is less than 20. If age is less than 20, the condition evaluates to True; otherwise, it evaluates to False.
-# 4. x = 5, y = 10, x > 3; returns True, y < 5; returns False. This is because the comparison operator > checks if x is greater than 3, which is true, while the comparison operator < checks if y is less than 5, which is false.
-# 5. x = 5, y = 10, x > 3; returns True, y < 5; returns False. This is because the comparison operator > checks if x is greater than 3, which is true, while the comparison operator < checks if y is less than 5, which is false.
+# 3. A single = is for assigning a value to a variable. == is for comparing two values. if statements require a comparison (True/False question), so using = inside an if crashes Python.
+# 4. The whole statement is False (because and requires both to be True).
+# 5. The whole statement is True (because or only requires one to be True).
 # 6. The purpose of elif statement is to provide an additional condition to check if the previous if statement is false. It allows for multiple conditions to be checked in a single block of code, and only one of the conditions will be executed. If the if statement is false, the program will check the elif statement, and if that is also false, it will move on to the next elif statement or the else statement if there are no more elif statements.
 # 7. If you write an if statement in Python but forget to indent the block of code beneath it, Python will immediately stop running your script and throw a IndentationError.
-# 8. print(10 % 2), print(11 % 2)
+# 8. number % 2 == 0 (This evaluates to True if the number is even, and False if it's odd).
 # 9. The output of the code will be: Good
 # 10. In backend development, operators and conditionals are the brain and the nervous system of your application. Without them, code is just a static script that executes a predictable list of instructions from top to bottom, completely identical for every user. With them, your backend transforms into an intelligent, dynamic engine capable of making real-time decisions.
 
