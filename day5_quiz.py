9d90eab5386cca70d97790cf63d15bdff4205516
pheetomilechi <tomilechighajiga@gmail.com>
2026-07-02 15:15:50 +0100

    completed day_5 quiz

---
+++ day5_quiz.py
@@ -0,0 +1,18 @@
+# 1. Loops are important in backend development because they allow developers to automate repetitive tasks, process large datasets, and implement algorithms efficiently. Loops enable the execution of a block of code multiple times based on certain conditions, which is essential for tasks such as iterating through database records, handling user input, and performing calculations. 
+# 2. Infinite loop is a loop that continues to execute indefinitely because the terminating condition is never set. It happens in a while loop when the condition always evaluates to true or in a for loop when the range is not properly defined. Infinite loops can lead to program crashes, unresponsiveness, and resource exhaustion, making them problematic in software development.
+# 3. range(3, 8) generates a sequence of numbers starting from 3 up to,  but not including 8. The output of range(3, 8) would be the numbers 3, 4, 5, 6, and 7.
+# 4. range(0, 10, 3) generates a sequence of numbers starting from 0 up to, but not including 10, with a step of 3. The output would be the numbers 0, 3, 6, and 9.
+# 5. The difference between for loop and while loop is that a for loop is used when the number of iterations is known beforehand, and it iterates over a sequence (like a list or range). 
+# A while loop, on the other hand, is used when the number of iterations is not known and continues to execute as long as a specified condition remains true. You make use of a for loop when you want to iterate over a collection of items or a specific range, while a while loop is more suitable for scenarios where the loop needs to continue until a certain condition changes.
+# 6. The break keyword in loop statements is used to exit the loop prematurely when a certain condition is met. 
+# 7. The continue keyword in loop statements is used to skip the current iteration of the loop and move on to the next iteration, effectively bypassing any code that follows it within the loop for that specific iteration.
+# 8. for i in range(4): 
+#       if i == 2: 
+#          continue 
+#       print(i) output (0, 1, 3) it skips 2 and go straight to 3.
+# 9. count = 0 
+#       while count < 3: 
+
+#    print("Hello") 
+#       count = count + 1 output (Hello, Hello, Hello) it prints Hello 3 times.
+# 10. We used break because the goal of the loop was to stop as soon as the user has successfully logged in.
