bfc8e5f9a08a60d1ab2a049ea9bcf3eec3f5cfcf
pheetomilechi <tomilechighajiga@gmail.com>
2026-07-06 12:02:00 +0100

    Created a day6_quiz file for quiz

---
+++ day6_quiz.py
@@ -0,0 +1,8 @@
+# 1. The symbol bracket is = []
+# 2. Python lists which are "0-indexed" means that the first element of a list is accessed with index 0, the second element with index 1.
+# 3. colors = ["Red", "Green", "Blue"], print(colors[-1]); output = Blue
+# 4. The difference between string and list regarding "immutability" is that strings are immutable, meaning they cannot be changed after they are created, while lists are mutable, meaning their elements can be modified. Thus they can be changed after they are created.
+# 5. The append() method adds an element to the end of a list. 
+# 6. insert(index, item) method allows you to insert an item at a specific index in the list, shifting the existing elements to the right.
+# 7. The difference between pop() and remove() methods is that pop() removes an element from a specific index and returns it, while remove() removes the first occurrence of a specified value from the list without returning it.
+# 8. users = ["Alice", "admin", "Bob"]
