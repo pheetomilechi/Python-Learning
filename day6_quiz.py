d7132f109c31ffed0b925772719a8816118101d8
pheetomilechi <tomilechighajiga@gmail.com>
2026-07-06 12:11:35 +0100

    Completed day6 quiz

--- day6_quiz.py
+++ day6_quiz.py
@@ -6,3 +6,11 @@
 # 6. insert(index, item) method allows you to insert an item at a specific index in the list, shifting the existing elements to the right.
 # 7. The difference between pop() and remove() methods is that pop() removes an element from a specific index and returns it, while remove() removes the first occurrence of a specified value from the list without returning it.
 # 8. users = ["Alice", "admin", "Bob"]
+#         if "admin" in users:
+#             print("True")
+# 9. The output of the code snippet is:
+#      nums = [10, 20, 30] 
+#      nums[1] = 99 
+#      print(nums); output = [10, 99, 30]
+# 10. for i in range(len(names)):
+#    print("Hello, " + names[i])
