dac09ecdef228fbc5679519b2e3f6b09c41eef5d
pheetomilechi <tomilechighajiga@gmail.com>
2026-07-06 09:25:56 +0100

    Practiced day 6 lecture inside project_day 6 file

---
+++ project_day6.py
@@ -0,0 +1,62 @@
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# usernames = ["Alice", "Bob", "Charlie", "David", "Eve"]
+# scores = [85, 92, 78, 90, 88]
+# mixed = ["Moses", 95, True]
+
+# fruits = ["apple", "banana", "cherry", "date"]
+
+# cars = ["Toyota", "Honda", "Ford", "Chevrolet"]
+# cars[1] = "Tesla"
+
+# animals = ["Cow", "Goat", "Fish", "Chicken"]
+# animals[2] = "Cat"
+
+# task = ["Code", "Eat"]
+# task.append("Sleep")
+
+# tasks = ["Code", "Eat"]
+# tasks.insert(1, "Meeting")
+
+# tasks = ["Code", "Eat", "Sleep"]
+# finished = tasks.pop()
+
+# tasks = ["Code", "Eat", "Sleep"]
+# tasks.remove("Eat")
+
+# # admins = ["Moses", "Promise", "Olivia"]
+# # input_admins = input("Enter your name: ")
+
+# # if input_admins in admins:
+# #      print("Access Granted!")
+   
+
+# fruits = ["Apple", "Banana", "Cherry"]
+
+# for fruit in fruits:
+#     print("I want to eat a " + fruit) 
+
