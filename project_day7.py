c459a5e2bb07b8233a06e7947916d974b77a8cf6
pheetomilechi <tomilechighajiga@gmail.com>
2026-07-08 11:46:37 +0100

    Complete day 7 project by building a "User Profile API Simulator". I used a dictionary called "profile" and added, updated and deleted keys from the dictionary which is in JSON format

--- project_day7.py
+++ project_day7.py
@@ -1,7 +1,75 @@
-user = {
-    "name": "Moses",
-    "age": 25,
-    "is_admin": False
+profile = {
+    "first_name": "Jude",
+    "last_name": "Dering",
+    "role": "Backend Developer",
+    "company": "Innovempia"
 }
 
-print(user["name"])
+profile["skills"] = "Python", "Git", "Dictionaries"
+
+profile["role"] = "Senior Backend Developer"
+
+# print(profile.get("email", "Email not found"))
+
+
+del profile ["company"]
+# print(profile)
+
+print("=== API USER PROFILE RESPONSE === ")
+
+for key, value in profile.items():
+    print("key: " + key + " value: " + str(value))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# user = {
+#     "name": "Moses",
+#     "age": 25,
+#     "is_admin": False
+# }
+
+# print(user["name"])
+# print(user.get("email"))
+
+# user["city"] = "Lagos"
+# user["age"] = 26
+
+
+# del user["city"]
+# print(user)
+
+# for key in user:
+#     print(key)
+    
+# for value in user.values():
+#     print(value)
+    
+# for key, value in user.items():
+    # print("The key is "+ key + " and the value is " + str(value))
