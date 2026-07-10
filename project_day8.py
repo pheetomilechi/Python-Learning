5b7452e44a99815f48056b9a1146ac647cf574fa
pheetomilechi <tomilechighajiga@gmail.com>
2026-07-10 09:04:06 +0100

    created a project day 8 file to practice day 8 and carryout day 8 project

---
+++ project_day8.py
@@ -0,0 +1,11 @@
+#user1
+price1 = 100
+tax1 = price1 * 0.10
+total1 = price1 + tax1
+print("Total is " + str(total1))
+
+#user2
+price2 = 50
+tax2 = price2 * 0.10
+total2 = price2 + tax2
+print("Total is " + str(total2))
