273c74ef7ec073b4ae5471878a075ab5ca3eb5b6
pheetomilechi <tomilechighajiga@gmail.com>
2026-07-10 10:24:19 +0100

    Completed day 8 lecture, which involved functions, such as return function and callback function heading right now to carry out day 8 project

--- project_day8.py
+++ project_day8.py
@@ -1,11 +1,81 @@
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 #user1
-price1 = 100
-tax1 = price1 * 0.10
-total1 = price1 + tax1
-print("Total is " + str(total1))
+# price1 = 100
+# tax1 = price1 * 0.10
+# total1 = price1 + tax1
+# print("Total is " + str(total1))
 
 #user2
-price2 = 50
-tax2 = price2 * 0.10
-total2 = price2 + tax2
-print("Total is " + str(total2))
+# price2 = 50
+# tax2 = price2 * 0.10
+# total2 = price2 + tax2
+# print("Total is " + str(total2))
+
+# def say_hello():
+#     # print("Hello, welcome to Innovempia!")
+#     # print("We are glad you are here.")
+    
+#     # print("Hello, welcome to Innovempia!")
+    
+#     def greet_user(name):
+#         print("Hello " + name + "!")
+        
+#     greet_user("Moses")
+#     greet_user("Promise")
+    
+    
+#     def introduce(name, role):
+#         print("Name: " + name)
+#         print("Role: " + role)
+#     introduce("Moses", "Backend Developer")
+    
+# def greet_user(name, company="Innovempia"):
+#         print("Hello, " + name + " from " + company)
+        
+# greet_user("Moses")
+
+# greet_user("Promise", "Google")
+
+# def add_numbers(num1, num2):
+#     total = num1 + num2
+#     print(total)
+    
+# add_numbers(5, 10)
+
+# my_money = add_numbers(5,10)
+# print(my_money)
+
+# def add_numbers(num1, num2):
+#     total = num1 + num2
+#     return total
+
+# my_money = add_numbers(5,10)
+# print("I have " + str(my_money) + " dollars. ")
