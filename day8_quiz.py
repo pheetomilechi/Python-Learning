7e1f94b2f0e5b72ff3091c64e7bb7d9751f12c8f
pheetomilechi <tomilechighajiga@gmail.com>
2026-07-15 09:48:00 +0100

    Answered question 1 to 10 i the quiz

---
+++ day8_quiz.py
@@ -0,0 +1,23 @@
+# 1. DRY principle stand for 'Don't Repeat Yourself' and it is important for backend systems because it prevents the mannual repetition of codes and it encourages writing code once and reusing it instead of copying the same logic multiple times.
+# 2. The keyword used to define (create) a function in puthon is 'def'.
+# 3. The difference between a parameter and an argument is that a parameter is the variable written inside the parantheses when you define the function while an argument is the actual value you pass inside the parantheses when you call the function.
+# 4. In this code; Alice is an argument
+# def greet(name)
+#      print(name)
+#  greet("Alice")
+# 5. The paramete will hold the default value "Admin" because python always fallback to the default value when no argument is passed.
+# 6. The fundamental difference between a print and return inside a function is that a print() shows text on the screen for human to read while a return() is how a function hands data back to the program so it can be saved to variables or used in calculations.
+# 7. def do_math(x):
+#       x = x * 2
+#       return x
+#      result = do_math(5)
+#       print(result); The output of this code will be 10
+# 8. def do_math(x):
+#       x = x * 2
+#       return x
+#      result = do_math(5)
+#       print(result); The output of this code will be 10.
+# 9. As soon as python hits a return statemet the function immediately terminates operations.
+# 10. def multiply(a, b):
+#       total = a * b
+#       return total
