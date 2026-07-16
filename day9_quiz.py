6ad304ea41413eff0ebca66de6df7d603d929cb2
pheetomilechi <tomilechighajiga@gmail.com>
2026-07-16 14:15:39 +0100

    Answered day9 quiz

---
+++ day9_quiz.py
@@ -0,0 +1,20 @@
+# 1. A local variable is a variable created inside a fucntion while a global variable is a variable created outside a function.
+# 2. Yes, a function can read a global variable and if it tries to modify it, it works only if you use a global keyword.
+# 3. Global keyword is used inside a function to modify a global variable.
+# 4. Unhandled error handling is dangerous in backend development because it crashes the processes and corrupts data.
+# 5. The two keywords in error handling is "ValueError" and "ZeroDivisionError".
+# 6. try:
+#       "hello" = int()
+#     except:
+#       print("Conversion failed")
+# 7. Except block tends to catch the error from the risky code and runs it without killing the program while else block runs only if the try block succeeds.
+# 8. The finally block runs the risky code whether the code succeeds or crashes.
+# 9. try:
+#       x = 10 / 0
+#    except ZeroDivisionError:
+#       print("A")
+#     else:
+#       print("B")
+#     finally:
+#       print("C"): Output = A & C
+# 10. False
