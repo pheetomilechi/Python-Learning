d167181eb12299d3b90fc0654a96a77f1700ea8c
pheetomilechi <tomilechighajiga@gmail.com>
2026-07-16 09:52:05 +0100

    updated the print statement for Q3 in the day7 quiz

--- day7_quiz.py
+++ day7_quiz.py
@@ -1,7 +1,7 @@
 # 1. The curly braces {} is used for dictionaries while parantheses () is used for tuples.
-# 2. In dictionary, a key is used to access a value while a value is used to access an index.
+# 2. A Key is the label (the left side). A Value is the data (the right side). You always query using the label.
 # 3. data = {"status": "active"}
-#               print(data["active"])
+#               print(data["status"])
 # 4. The main danger of accessing dictionary data using bracket notation , data["email"] is that if the key does not exist, it will raise a KeyError. 
 # 5. The .get() method is used to retrieve the value associated with a key in a dictionary. It allows you to specify a default value to return if the key does not exist, preventing a KeyError from being raised.
 # 6. With user["age"] = 30, if the age already exist in the dictionary, it will modify the dictionary by overwritting the existing one.
