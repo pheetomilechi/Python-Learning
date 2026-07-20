76d43f8be0745ab5e502b1b42e769cf58cbcb09a
pheetomilechi <tomilechighajiga@gmail.com>
2026-07-20 09:36:12 +0100

    Practiced the use of modules by trying import statements from various modules like math, randint, datetime and requests libraries and also the use of as keyword

---
+++ project_day10.py
@@ -0,0 +1,25 @@
+# import math
+# print(math.sqrt(16))
+
+# from random import randint
+# print(randint(1, 10))
+
+# import datetime as dt
+# print(dt.datetime.now())
+
+# import datetime
+
+# right_now = datetime.datetime.now()
+# print("Year: " + str(right_now.year))
+# print("Month: " + str(right_now.month))
+
+# import requests
+# response = requests.get("https://jsonplaceholder.typicode.com/users/1")
+
+# user_data = response.json()
+
+# name = user_data["name"]
+# email = user_data["email"]
+
+# print("Name: " + name)
+# print("Email: " + email)
