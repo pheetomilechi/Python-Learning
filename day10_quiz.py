1c2e60435fbf4496098465a767d272c18018e25c
pheetomilechi <tomilechighajiga@gmail.com>
2026-07-21 19:58:06 +0100

    Completed day 1o quiz by answering 10 questions

---
+++ day10_quiz.py
@@ -0,0 +1,12 @@
+# 1. A module is any file that ends with .py
+# 2. The difference between "import math" and "from math import sqrt" is that import math loads the python file into memory so i can use the math variables and functions while from import can import just the specific component you need.
+# 3. The as keyword shortens the module names that are long. E.g; 
+# import datetime as dt
+# 4. Python Standard Libray are built-in pre-written modules that are imported.
+# 5. PyPI is known as Python Package Index which is a massive online registry of free code written by global developer community.
+# 6. The tool or command used to download or install packages from PYPI is "pip install"
+# 7. The command used to install external package called flask "pip install flask" for windows and "pip3 install flask" for Mac/Linux.
+# 8. In the requests libray, .json() method converts your raw JSON string to a Python dictionary.
+# 9. import calculations
+#       add()
+# 10. Because everything learned about installing and importing requests is the exact same process used to install Flask/Django
