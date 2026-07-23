538586c34f93daffdf1abff66500f66973df3aab
pheetomilechi <tomilechighajiga@gmail.com>
2026-07-23 15:05:00 +0100

    writing and appending files using "w"

--- project_day11.py
+++ project_day11.py
@@ -3,7 +3,17 @@
 # print(content)
 # file.close()
 
-file = open("sample.txt", "r")
-line1 = file.readline()
-print(line1)
+# file = open("sample.txt", "r")
+# line1 = file.readline()
+# print(line1)
+# file.close()
+
+# file = open("sample.txt", "r")
+# lines_list = file.readlines()
+# print(lines_list)
+# file.close()
+
+file = open("output.txt", "w")
+file.write("This is the first line. \n")
+file.write("This is the second line.")
 file.close()
